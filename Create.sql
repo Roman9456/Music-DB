e97f0f3643f8fbc9ee0e5d017bb223c3d5933e9e
Roman Smolyakov <123468411+Roman9456@users.noreply.github.com>
2023-07-07 13:49:29 +0300

    Update Create.sql

--- Create.sql
+++ Create.sql
@@ -10,27 +10,22 @@ CREATE TABLE IF NOT EXISTS Artists (
   name VARCHAR(255) UNIQUE
 );
 
--- Создание таблицы ArtistGenres
-CREATE TABLE IF NOT EXISTS ArtistGenres (
-  artist_id INTEGER REFERENCES Artists(artist_id),
-  genre_id INTEGER REFERENCES Genres(genre_id),
-  PRIMARY KEY (artist_id, genre_id)
-);
-
 -- Создание таблицы Albums
 CREATE TABLE IF NOT EXISTS Albums (
   album_id SERIAL PRIMARY KEY,
-  title VARCHAR(255) UNIQUE,
+  title VARCHAR(255),
   year INTEGER CHECK (year >= 1900),
-  artist_id INTEGER REFERENCES Artists(artist_id)
+  artist_id INTEGER REFERENCES Artists(artist_id),
+  CONSTRAINT unique_album_title_artist_id UNIQUE (title, artist_id)
 );
 
 -- Создание таблицы Tracks
 CREATE TABLE IF NOT EXISTS Tracks (
   track_id SERIAL PRIMARY KEY,
-  title VARCHAR(255) UNIQUE,
+  title VARCHAR(255),
   duration INTEGER CHECK (duration > 0),
-  album_id INTEGER REFERENCES Albums(album_id)
+  album_id INTEGER REFERENCES Albums(album_id),
+  CONSTRAINT unique_track_title UNIQUE (title)
 );
 
 -- Создание таблицы Collections
@@ -41,8 +36,29 @@ CREATE TABLE IF NOT EXISTS Collections (
   CONSTRAINT unique_collection_title UNIQUE (title)
 );
 
+-- Создание таблицы ArtistGenres
+CREATE TABLE IF NOT EXISTS ArtistGenres (
+  artist_id INTEGER REFERENCES Artists(artist_id),
+  genre_id INTEGER REFERENCES Genres(genre_id),
+  PRIMARY KEY (artist_id, genre_id)
+);
+
+CREATE TABLE IF NOT EXISTS AlbumArtists (
+  album_id INTEGER REFERENCES Albums(album_id),
+  artist_id INTEGER REFERENCES Artists(artist_id),
+  PRIMARY KEY (album_id, artist_id)
+);
+
 -- Создание таблицы CollectionTracks
 CREATE TABLE IF NOT EXISTS CollectionTracks (
   collection_id INTEGER REFERENCES Collections(collection_id),
   track_id INTEGER REFERENCES Tracks(track_id),
   PRIMARY KEY (collection_id, track_id)
+);
+
+-- Создание таблицы ArtistAlbums
+CREATE TABLE IF NOT EXISTS ArtistAlbums (
+  artist_id INTEGER REFERENCES Artists(artist_id),
+  album_id INTEGER REFERENCES Albums(album_id),
+  PRIMARY KEY (artist_id, album_id)
+);
